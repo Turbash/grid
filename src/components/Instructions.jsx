c484d7bc883a1d85c60d8b1a64ed7970d8908f8c
Turbash <negirawatdeepi@gmail.com>
2025-11-05 19:25:08 +0530

    UI done now time to add playground functionality

--- src/components/Instructions.jsx
+++ src/components/Instructions.jsx
@@ -2,14 +2,13 @@ import React from "react";
 
 const Instructions = ({ text }) => {
   return (
-        <p className="mx-auto my-12 max-w-xl text-white">
-          <span className="font-fredoka text-lg tracking-wide text-white">
-            Instructions:
-          </span>
-          Arrange the apples in the red basket and the grapes in the purple
-          basket using the 'order' and 'align-self' properties of flex-box on
-          the apple.
-        </p>
+    <p className="mx-auto my-12 max-w-xl text-white">
+      <span className="font-fredoka text-lg tracking-wide text-white">
+        Instructions:
+      </span>
+      Arrange the apples in the red basket and the grapes in the purple basket
+      using the 'order' and 'align-self' properties of flex-box on the apple.
+    </p>
   );
 };
 
--- src/components/Playground.jsx
+++ src/components/Playground.jsx
@@ -1,86 +1,97 @@
 import React from "react";
 import background from "../assets/background.webp";
+import apple from "../assets/apple.webp";
+import grapes from "../assets/grapes.webp";
+import banana from "../assets/banana.webp";
+import red_basket from "../assets/red_basket.webp";
+import violet_basket from "../assets/violet_basket.webp";
+import yellow_basket from "../assets/yellow_basket.webp";
 
 const Playground = () => {
   return (
-    <div className="p-16">
+    <div className="p-16 relative">
       <div
         className="relative mx-auto h-[300px] w-[300px] rounded-xl bg-cover lg:h-[500px] lg:w-[500px] xl:h-[550px] xl:w-[550px]"
-        style={{ backgroundImage: `url("${background}")`, backgroundPosition: "center center" }}
+        style={{
+          backgroundImage: `url("${background}")`,
+          backgroundPosition: "center center",
+        }}
       >
-        <img src={background} alt="" className="" />
         <div className="relative mx-auto h-full w-[98%] max-w-[1000px] lg:ml-0 lg:mr-auto">
           <div id="container" className="flex h-full px-16 py-20 lg:p-24">
             <div id="apple" className="h-10 w-10 lg:h-[70px] lg:w-[70px]">
               <img
                 alt="fruit"
                 className="mx-auto h-[70%] w-[70%] animate-bounce drop-shadow-xl"
-                src="https://cdn.codehelp.in/fruitbox-flex/apple.webp"
+                src={apple}
               />
             </div>
             <div id="grapes" className="h-10 w-10 lg:h-[70px] lg:w-[70px]">
               <img
                 alt="fruit"
                 className="mx-auto h-[70%] w-[70%] animate-bounce drop-shadow-xl"
-                src="https://cdn.codehelp.in/fruitbox-flex/grapes.webp"
+                src={grapes}
               />
             </div>
             <div id="apple" className="h-10 w-10 lg:h-[70px] lg:w-[70px]">
               <img
                 alt="fruit"
                 className="mx-auto h-[70%] w-[70%] animate-bounce drop-shadow-xl"
-                src="https://cdn.codehelp.in/fruitbox-flex/apple.webp"
+                src={apple}
               />
             </div>
             <div id="grapes" className="h-10 w-10 lg:h-[70px] lg:w-[70px]">
               <img
                 alt="fruit"
                 className="mx-auto h-[70%] w-[70%] animate-bounce drop-shadow-xl"
-                src="https://cdn.codehelp.in/fruitbox-flex/grapes.webp"
+                src={grapes}
               />
             </div>
             <div id="grapes" className="h-10 w-10 lg:h-[70px] lg:w-[70px]">
               <img
                 alt="fruit"
                 className="mx-auto h-[70%] w-[70%] animate-bounce drop-shadow-xl"
-                src="https://cdn.codehelp.in/fruitbox-flex/grapes.webp"
+                src={grapes}
               />
             </div>
           </div>
-          <div id="master" className="absolute inset-0 flex px-16 py-20 lg:p-24">
+          <div
+            id="master"
+            className="absolute inset-0 flex px-16 py-20 lg:p-24"
+          >
             <div id="red" className="h-10 w-10 lg:h-[70px] lg:w-[70px]">
               <img
                 alt="basket"
                 className="animate-pulse drop-shadow-2xl"
-                src="https://cdn.codehelp.in/fruitbox-flex/red_basket.webp"
+                src={red_basket}
               />
             </div>
             <div id="violet" className="h-10 w-10 lg:h-[70px] lg:w-[70px]">
               <img
                 alt="basket"
                 className="animate-pulse drop-shadow-2xl"
-                src="https://cdn.codehelp.in/fruitbox-flex/violet_basket.webp"
+                src={violet_basket}
               />
             </div>
             <div id="red" className="h-10 w-10 lg:h-[70px] lg:w-[70px]">
               <img
                 alt="basket"
                 className="animate-pulse drop-shadow-2xl"
-                src="https://cdn.codehelp.in/fruitbox-flex/red_basket.webp"
+                src={red_basket}
               />
             </div>
             <div id="violet" className="h-10 w-10 lg:h-[70px] lg:w-[70px]">
               <img
                 alt="basket"
                 className="animate-pulse drop-shadow-2xl"
-                src="https://cdn.codehelp.in/fruitbox-flex/violet_basket.webp"
+                src={violet_basket}
               />
             </div>
             <div id="violet" className="h-10 w-10 lg:h-[70px] lg:w-[70px]">
               <img
                 alt="basket"
                 className="animate-pulse drop-shadow-2xl"
-                src="https://cdn.codehelp.in/fruitbox-flex/violet_basket.webp"
+                src={violet_basket}
               />
             </div>
           </div>
